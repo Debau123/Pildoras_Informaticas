fb83354d99c114d8e9d1ea0dc587ce6310ecf67b
iñaki <debau@hotmail.es>
2023-10-18 16:22:26 +0200

    restructuracion

---
+++ untitled/src/Raices.java
@@ -0,0 +1,12 @@
+
+
+public class Raices {
+    public static void main(String[] args) {
+ //int raiz=(int)Math.sqrt(9);
+     double base=5;
+     double exponente=3;
+     int resultado=(int)Math.pow(base , exponente);
+
+      System.out.println(resultado);
+    }
+}
